7fcece12f52efd8041b56e13fac6cebc242f834b
Subhranil Chakraborty <106914208+subhranil002@users.noreply.github.com>
2022-12-15 22:58:21 +0530

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,7 @@
+#Comment Style 1
+""" 
+Author --> Subhranil
+Hello Viewer
+""" 
+#Comment Style 2
+print("Hey, I am Subhranil") #Greeting 
---
+++ Print_Function.py
@@ -0,0 +1,4 @@
+# sep = Separater
+# end = Prints at the end of the line
+print("Hey", 6, 7,sep="-",end="~HeHe\n")
+print("Hey", 6, 7)
