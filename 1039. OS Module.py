b83fbb25e0a7efad15d110fad60b455498866f2f
Subhranil Chakraborty <106914208+subhranil002@users.noreply.github.com>
2023-01-16 23:05:18 +0530

    Add files via upload

---
+++ 1039. OS Module.py
@@ -0,0 +1,15 @@
+import os
+
+# if(not os.path.exists("data")):
+#     os.mkdir("data") # Creates a folder named "data"
+
+# for i in range(0,100):
+#     os.mkdir(f"data/Day{i+1}") # Creates 100 folders in "data" folder
+
+# for i in range(0,100):
+#     os.rename(f"data/Day{i+1}", f"data/Tutorial{i+1}") # Renames all folders from f"data/Day{i+1}" to f"data/Tutorial{i+1}"
+
+# folders = os.listdir("data")
+# print(folders) # Prints name of all the folders
+
+# And Many More ... 
