aebb910c0fdacf8e5622dad6562324dfe10675b4
Subhranil Chakraborty <106914208+subhranil002@users.noreply.github.com>
2023-01-19 02:24:14 +0530

    Add files via upload

---
+++ 1040. Local & Global Variable.py
@@ -0,0 +1,31 @@
+x = 10  # global variable
+
+
+def my_function():
+    y = 5  # local variable
+    x = 55  # local variable
+    print(y)  # 5 # Value of Local Variable
+    print(x)  # 55 # Value of Local Variable
+
+
+my_function()
+
+print(x)  # 10 # Value of Global Variable
+# print(y) # this will cause an error because y is a local variable
+# and is not accessible outside of the function
+
+
+# To Change Global Variable -->
+x = 10  # global variable
+
+
+def my_function():
+    global x
+    x = 5  # this will change the value of the global variable x
+    y = 5  # local variable
+
+
+my_function()
+print(x)  # prints 5
+print(y)  # this will cause an error because y is a local variable
+# and is not accessible outside of the function
