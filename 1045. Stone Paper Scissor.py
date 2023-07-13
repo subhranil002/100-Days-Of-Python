a75513fe3a684e0584a9749e03f5dc1f5ca45855
Subhranil Chakraborty <106914208+subhranil002@users.noreply.github.com>
2023-07-14 01:38:42 +0530

    Update and rename 1045. Snake Water Gun.py to 1045. Stone Paper Scissor.py

---
+++ 1045. Stone Paper Scissor.py
@@ -0,0 +1,44 @@
+import random
+
+
+def check(comp, user):
+    if comp == user:
+        return 0
+
+    if (comp == 0 and user == 1):
+        return -1
+
+    if (comp == 1 and user == 2):
+        return -1
+
+    if (comp == 2 and user == 0):
+        return -1
+
+    return 1
+
+
+totalscore = 0
+print("Enter 3 to exit")
+
+while True:
+    user = int(input("0 for Stone, 1 for Paper and 2 for Scissor:\n"))
+
+    if user == 3:
+        print(f"Total Score: {totalscore}")
+        break
+
+    comp = random.randint(0, 2)
+
+    print(f"Computer's choice: {comp}")
+
+    score = check(comp, user)
+
+    if (score == 0):
+        print("Its a draw")
+    elif (score == -1):
+        print("You Win")
+        totalscore += 1
+    else:
+        print("You Lose")
+        if totalscore != 0:
+            totalscore -= 1
